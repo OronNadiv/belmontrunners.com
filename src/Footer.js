49faae1ae0aa854ef30a3cb32c8919866569ea9f
Oron Nadiv <oron@nadiv.us>
2019-02-16 16:47:48 -0800

    footer

--- src/Footer.js
+++ src/Footer.js
@@ -6,11 +6,12 @@ class Footer extends Component {
       <footer className='footer-area pad_btm'>
         <div className='container'>
           <div className='row footer-bottom d-flex justify-content-between align-items-center'>
-            <p className='col-lg-8 col-md-8 footer-text m-0'>
-              Copyright &copy;{new Date().getFullYear()} All rights reserved | This template is made with <i
-              className='far fa-heart'
-              aria-hidden='true' /> by <a
-              href='https://colorlib.com' target='_blank' rel='noopener noreferrer'>Colorlib</a>
+            <p className='col-lg-8 col-md-8 m-0 footer-text'>
+              This website was built with <i style={{ color: 'red' }}
+                                             className='fas fa-heart '
+                                             aria-hidden='true' /> by <a
+              href='https://www.oronnadiv.com' target='_blank' rel='noopener noreferrer'>Oron
+              Nadiv</a>
             </p>
             <div className='col-lg-4 col-md-4 footer-social'>
               <a target='_blank' rel='noopener noreferrer' href='https://www.facebook.com/belmontrunnersclub/'>
