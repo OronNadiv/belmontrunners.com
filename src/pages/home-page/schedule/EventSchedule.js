bf1c1e64401a0d038e83e5c89f8d1821859db8f8
Oron Nadiv <oron@nadiv.us>
2019-06-22 23:47:54 -0700

    1. Add support for "load more". 2. Add tutorial for iphone/ipad.

--- src/pages/home-page/schedule/EventSchedule.js
+++ src/pages/home-page/schedule/EventSchedule.js
@@ -1,108 +1,134 @@
-import React, { Component } from 'react'
+import React, { useEffect, useState } from 'react'
 import request from 'request'
 import csv from 'csvtojson'
 import moment from 'moment'
 import CalendarSelector from './CalendarSelector'
+import ExpendMoreIcon from '@material-ui/icons/ExpandMore'
+import IconButton from '@material-ui/core/IconButton'
 
-class EventSchedule extends Component {
-  constructor (props) {
-    super(props)
-    this.state = { events: [] }
-  }
+const SPREADSHEET_URL = "https://docs.google.com/spreadsheets/d/1FZOB291KWLoutpr0s6VeK5EtvuiQ8uhe497nOmWoqPA/export?format=csv&usp=sharing"
 
-  async componentDidMount () {
-    let events = await csv()
-      .fromStream(request.get("https://docs.google.com/spreadsheets/d/1FZOB291KWLoutpr0s6VeK5EtvuiQ8uhe497nOmWoqPA/export?format=csv&usp=sharing"))
-    events = events.map(event => {
-      event.month--
-      event.moment = moment(event)
-      return event
-    })
-      .filter(event => {
-        return event.moment.isAfter(moment().subtract(1, 'day')) &&
-          event.moment.isBefore(moment().add(15, 'day'))
-      })
-      .sort((a, b) => {
-        return a.moment.valueOf() - b.moment.valueOf()
-      })
-
-    this.setState({ events })
-  }
-
-  render () {
-    return (
-      <section className="event_schedule_area pad_btm">
-        <div className="container">
-          <div className="main_title">
-            <h2>Upcoming Events</h2>
-          </div>
-          <div className='d-flex flex-row-reverse mb-3'>
-            <CalendarSelector />
-          </div>
-          <div className="event_schedule_inner">
-            <div className="tab-content" id="myTabContent">
-              <div className="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
-                {
-                  this.state.events
-                    .map((event, index) => {
-                        return (
-                          <div key={index} className="media">
-                            <div className="d-flex">
-                              <img src="img/schedule/schedule-3.png" alt="" />
-                            </div>
-                            <div className="media-body">
-                              <h5>{event.moment.format('MMMM D h:mm a')}</h5>
-                              <h4
-                                className={event['is-special-event'] === 'TRUE' ? 'special-event' : undefined}>
-                                {event.moment.format("dddd")} {event.subject}
-                              </h4>
-                              <p>What: {event.what}</p>
-                              <p>Where: {event.where}</p>
-                              {
-                                (event['google-map-id'] || event['facebook-event-id']) ?
-                                  <div className='d-flex flex-wrap'>
-                                    {
-                                      event['google-map-id'] &&
-                                      EventSchedule.getMapLink(event['google-map-id'])
-                                    }
-                                    {
-                                      event['facebook-event-id'] &&
-                                      EventSchedule.getFacebookEventLink(event['facebook-event-id'])
-                                    }
-                                  </div>
-                                  : <span />
-                              }
-                            </div>
-                            {
-                              event['is-members-only-event'] === 'TRUE' &&
-                              <img src="img/schedule/members-only-t.png" alt="" style={{ width: 58 }} />
-                            }
-                          </div>
-                        )
-                      }
-                    )
-                }
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-    )
-  }
-
-  static getMapLink (eventElement) {
-    return <span style={{ paddingRight: "1em" }}>
+const getMapLink = (eventElement) => {
+  return <span style={{ paddingRight: "1em" }}>
     <a target="_blank" rel="noopener noreferrer" href={`https://goo.gl/maps/${eventElement}`}>
     <i className="fas fa-map-marker-alt" />&nbsp;Meeting Point</a>
     </span>
-  }
+}
 
-  static getFacebookEventLink (eventElement) {
-    return <span>
+const getFacebookEventLink = (eventElement) => {
+  return <span>
     <a target="_blank" rel="noopener noreferrer" href={`https://www.facebook.com/events/${eventElement}`}>
     <i className="fab fa-facebook-square" />&nbsp;Facebook Event</a>
     </span>
-  }
+}
+
+function EventSchedule () {
+  const [events, setEvents] = useState([])
+  const [filteredEvents, setFilteredEvents] = useState([])
+  const [daysAhead, setDaysAhead] = useState(15)
+  const [loadMoreClicked, setLoadMoreClicked] = useState(0)
+
+  useEffect(() => {
+    (async function () {
+      const allEvents = await csv().fromStream(request.get(SPREADSHEET_URL))
+      const events = allEvents
+        .map(event => {
+          event.month--
+          event.moment = moment(event)
+          return event
+        })
+        .filter(event => {
+          return event.moment.isValid() && event.moment.isAfter(moment().subtract(1, 'day'))
+        })
+        .sort((a, b) => {
+          return a.moment.valueOf() - b.moment.valueOf()
+        })
+      setEvents(events)
+    })()
+  }, [])
+
+  useEffect(() => {
+    (async function () {
+      console.log('events:', events)
+      const filteredEvents = events
+        .filter(event => {
+          return event.moment.isBefore(moment().add(daysAhead, 'day'))
+        })
+      setFilteredEvents(filteredEvents)
+    })()
+  }, [events, daysAhead])
+
+  return (
+    <section className="event_schedule_area pad_btm">
+      <div className="container">
+        <div className="main_title">
+          <h2>Upcoming Events</h2>
+        </div>
+        <div className='d-flex flex-row-reverse mb-3'>
+          <CalendarSelector />
+        </div>
+        <div className="event_schedule_inner">
+          <div className="tab-content" id="myTabContent">
+            <div className="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+              {
+                filteredEvents
+                  .map((event, index) => {
+                      return (
+                        <div key={index} className="media">
+                          <div className="d-flex">
+                            <img src="img/schedule/schedule-3.png" alt="" />
+                          </div>
+                          <div className="media-body">
+                            <h5>{event.moment.format('MMMM D h:mm a')}</h5>
+                            <h4
+                              className={event['is-special-event'] === 'TRUE' ? 'special-event' : undefined}>
+                              {event.moment.format("dddd")} {event.subject}
+                            </h4>
+                            <p>What: {event.what}</p>
+                            <p>Where: {event.where}</p>
+                            {
+                              (event['google-map-id'] || event['facebook-event-id']) ?
+                                <div className='d-flex flex-wrap'>
+                                  {
+                                    event['google-map-id'] &&
+                                    getMapLink(event['google-map-id'])
+                                  }
+                                  {
+                                    event['facebook-event-id'] &&
+                                    getFacebookEventLink(event['facebook-event-id'])
+                                  }
+                                </div>
+                                : <span />
+                            }
+                          </div>
+                          {
+                            event['is-members-only-event'] === 'TRUE' &&
+                            <img src="img/schedule/members-only-t.png" alt="" style={{ width: 58 }} />
+                          }
+                        </div>
+                      )
+                    }
+                  )
+              }
+            </div>
+          </div>
+        </div>
+        {
+          loadMoreClicked <= 2 && filteredEvents.length < events.length &&
+          <div className='d-flex justify-content-center'>
+            <IconButton
+              onClick={() => {
+                setLoadMoreClicked(loadMoreClicked + 1)
+                setDaysAhead(daysAhead + 7)
+              }}
+            >
+              <ExpendMoreIcon />
+            </IconButton>
+          </div>
+        }
+      </div>
+    </section>
+  )
 }
 
 export default EventSchedule
--- src/pages/home-page/schedule/IosIcon.js
+++ src/pages/home-page/schedule/IosIcon.js
@@ -5,24 +5,11 @@ function IosIcon () {
   return (
     <div>
       <SvgIcon width={24} height={24}>
-        <svg xmlns='http://www.w3.org/2000/svg' width='32' height='32' viewBox='0 0 32 32'>
-          <g transform='translate(-754 -954.362)'>
-            <path fill='none' stroke='#576d7e' strokeLinecap='round' strokeLinejoin='round'
-                  d='M779.09996 966.36933l0 .5446M779.09996 962.36933l0 2.0686' />
-            <rect width='18' height='31' x='761' y='955.362' fill='#e9eded' rx='1.421'
-                  ry='1.5' />
-            <ellipse cx='767.998' cy='957.365' fill='#576d7e' rx='0.499' ry='0.498'
-            />
-            <path fill='#576d7e' fillRule='evenodd' stroke='#576d7e' strokeLinecap='round'
-                  strokeLinejoin='round' d='m 770.00311,957.33953 1.99103,0' />
-            <circle cx='770' cy='983.862' r='0.5' fill='#576d7e' stroke='#576d7e'
-                    strokeLinecap='round' strokeLinejoin='round' />
-            <rect width='14' height='22' x='763' y='959.362' fill='#4bbfeb' rx='0'
-                  ry='0' />
-            <path fill='#cad1d8'
-                  d='m 761,-36.5 0,1 c 0,0.83101 0.63461,1.5 1.42188,1.5 l 15.15624,0 C 778.36539,-34 779,-34.66899 779,-35.5 l 0,-1 c 0,0.83101 -0.63461,1.5 -1.42188,1.5 l -15.15624,0 C 761.63461,-35 761,-35.66899 761,-36.5 Z'
-                  transform='translate(0 1020.362)' />
-          </g>
+        <svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='#02A8F4'>
+          <path
+            d='M15.5 1h-8C6.12 1 5 2.12 5 3.5v17C5 21.88 6.12 23 7.5 23h8c1.38 0 2.5-1.12 2.5-2.5v-17C18 2.12 16.88 1 15.5 1zm-4 21c-.83 0-1.5-.67-1.5-1.5s.67-1.5 1.5-1.5 1.5.67 1.5 1.5-.67 1.5-1.5 1.5zm4.5-4H7V4h9v14z'
+          />
+          <path d='M0 0h24v24H0z' fill='none' />
         </svg>
       </SvgIcon>
     </div>
