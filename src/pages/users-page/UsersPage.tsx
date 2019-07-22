3777b60b92c9b859db2d086852cb27da825b2684
Oron Nadiv <oron@nadiv.us>
2019-07-21 20:57:44 -0700

    users-page2 -> users-page. Remove redundant sentry alert.

--- src/pages/users-page/UsersPage.tsx
+++ src/pages/users-page/UsersPage.tsx
@@ -245,7 +245,7 @@ function UsersPage (props: UsersPageProps) {
             }
             const userData = _.findWhere(rows, { [UID]: tableMeta.rowData[0] }) as IUserDataExtended
             if (!userData) {
-              throw new Error(`userData is null. tableMeta: ${JSON.stringify(tableMeta)}`)
+              return value
             }
 
             return (
