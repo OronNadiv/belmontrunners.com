baf7a6756c4695cb4c0da709c11dd54223acc47a
Oron Nadiv <oron@nadiv.us>
2019-12-15 10:43:47 -0800

    Logs

--- functions/contacts2MailChimp.js
+++ functions/contacts2MailChimp.js
@@ -7,6 +7,8 @@ const rp = require('request-promise')
 const Promise = require('bluebird')
 const md5 = require('md5')
 
+console.log('functions.config().mailchimp.apikey:', Boolean(apiKey))
+
 module.exports = admin => {
   const firestore = admin.firestore()
 
@@ -36,52 +38,56 @@ module.exports = admin => {
     // }
 
     const syncActive = async () => {
-      const body = active.map(contact => {
-        const mailChimpContact = {
-          email_address: contact[EMAIL]
-        }
-        const displayName = contact[DISPLAY_NAME]
-        if (displayName) {
-          const name = parseFullName(displayName)
-          mailChimpContact.merge_fields = {
-            FNAME: name.first || '',
-            LNAME: name.last || ''
+      try {
+        const body = active.map(contact => {
+          const mailChimpContact = {
+            email_address: contact[EMAIL]
           }
-        }
-        return mailChimpContact
-      })
-      await Promise.each(body, async item => {
-        try {
-          await rp({
-            method: 'POST',
-            uri: `https://username:${apiKey}@us3.api.mailchimp.com/3.0/lists/7cffd16da0/members/`,
-            body: { ...item, status: 'subscribed' },
-            json: true
-          })
-          console.info('done POST:', item.email_address)
-        } catch (err) {
-          if (err && err.error && err.error.status === 400) {
-            try {
-              await rp({
-                method: 'PUT',
-                uri: `https://username:${apiKey}@us3.api.mailchimp.com/3.0/lists/7cffd16da0/members/${md5(
-                  item.email_address.toLowerCase()
-                )}`,
-                body: item,
-                json: true
-              })
-              console.info('done PUT:', item.email_address)
-            } catch (err2) {
-              console.error(
-                'error PUT:',
-                err2 && err2.error ? err2.error : err2
-              )
+          const displayName = contact[DISPLAY_NAME]
+          if (displayName) {
+            const name = parseFullName(displayName)
+            mailChimpContact.merge_fields = {
+              FNAME: name.first || '',
+              LNAME: name.last || ''
             }
-          } else {
-            console.error('error POST:', err && err.error ? err.error : err)
           }
-        }
-      })
+          return mailChimpContact
+        })
+        await Promise.each(body, async item => {
+          try {
+            await rp({
+              method: 'POST',
+              uri: `https://username:${apiKey}@us3.api.mailchimp.com/3.0/lists/7cffd16da0/members/`,
+              body: { ...item, status: 'subscribed' },
+              json: true
+            })
+            console.info('done POST:', item.email_address)
+          } catch (err) {
+            if (err && err.error && err.error.status === 400) {
+              try {
+                await rp({
+                  method: 'PUT',
+                  uri: `https://username:${apiKey}@us3.api.mailchimp.com/3.0/lists/7cffd16da0/members/${md5(
+                    item.email_address.toLowerCase()
+                  )}`,
+                  body: item,
+                  json: true
+                })
+                console.info('done PUT:', item.email_address)
+              } catch (err2) {
+                console.error(
+                  'error PUT:',
+                  err2 && err2.error ? err2.error : err2
+                )
+              }
+            } else {
+              console.error('error POST:', err && err.error ? err.error : err)
+            }
+          }
+        })
+      } catch (err) {
+        console.error('in syncActive. err:', err)
+      }
     }
 
     await syncActive()
