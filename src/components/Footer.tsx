41274ae98a454aff8e18d0dc126f05bc31f4c104
Oron Nadiv <oron@nadiv.us>
2021-06-17 01:11:35 -0700

    Remove IG scrapping

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,24 +1,6 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { makeStyles } from '@material-ui/core'
 import { BLOG } from '../urls'
-
-const ig = require('instagram-scraping')
-
-interface ISource {
-  src: string
-}
-
-interface IMedia {
-  media_id: string
-  thumbnail_resource: ISource[]
-  text: string
-}
-
-interface IFeed {
-  medias: IMedia[]
-}
-
-const initialFeed: IFeed | undefined = undefined
 
 function Footer() {
   const useStyles = makeStyles(() => ({
@@ -28,61 +10,11 @@ function Footer() {
   }))
   const classes = useStyles()
 
-  const [feed, setFeed] = useState(initialFeed)
-
-  useEffect(() => {
-    ig.scrapeUserPage('belmontrunners')
-      .then((result: IFeed) => {
-        setFeed(result)
-      })
-      .catch((err: any) => {
-        console.warn('failed to scrape Instagram feed.  error:', err)
-      })
-  }, [])
-
-  const getRssFeed = () => {
-    if (!feed) {
-      return null
-    }
-    const getImage = (media: IMedia) => {
-      return (
-        <li key={media.media_id}>
-          <a
-            href="https://www.instagram.com/belmontrunners/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              style={{ height: 58, width: 58 }}
-              src={media.thumbnail_resource[0].src}
-              alt={media.text}
-            />
-          </a>
-        </li>
-      )
-    }
-    return (
-      <div className="single-footer-widget instafeed">
-        <h6 className="footer_title text-center">Instagram Feed</h6>
-        <ul className="list instafeed d-flex flex-wrap justify-content-center">
-          {feed.medias.slice(0, 4).map(media => {
-            return getImage(media)
-          })}
-        </ul>
-        <ul className="list instafeed d-flex flex-wrap justify-content-center">
-          {feed.medias.slice(4, 8).map(media => {
-            return getImage(media)
-          })}
-        </ul>
-      </div>
-    )
-  }
-
   return (
     <footer className="footer-area pad_btm">
       <div className="container">
         <div className="row footer-bottom d-flex justify-content-between text-center align-items-center">
-          <div className="col-lg-4 col-md-12 footer-social">
+          <div className="col-lg-4 col-12 mb-lg-0 mb-4 footer-social">
             <a
               target="_blank"
               rel="noopener noreferrer"
@@ -112,8 +44,7 @@ function Footer() {
               <i className="fas fa-blog" />
             </a>
           </div>
-          <div className="col-lg-4 col-md-12 my-md-4">{getRssFeed()}</div>
-          <p className="col-lg-4 col-md-12 footer-text ">
+          <p className="col-lg-4 col-12 footer-text ">
             This website was built with{' '}
             <i className={`${classes.i} fas fa-heart`} aria-hidden="true" /> by{' '}
             <a
